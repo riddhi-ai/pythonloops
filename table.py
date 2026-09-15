464c185f69af1d9ef60bfc78910253c59f749753
Riddhi Naskari <riddhinaskari@gmail.com>
2026-09-15 10:07:39 +0530

    comments removed

--- table.py
+++ table.py
@@ -1,5 +1,5 @@
-num = 2         # the number you want the table for
-i = 1             # counter starts at 1
+num = 2         
+i = 1           
 
 while i <= 10:
     print(num, "x", i, "=", num * i)
